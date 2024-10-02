60e33dc3de507ab8ee16c3efd740c8f821de8ac6
DiegoNoriega00 <diegonoriega2005@gmail.com>
2024-10-02 13:00:50 -0600

    Se corrigió la posición de la imagen y texto de la pantalla de error, dado que aparecían desordenadas

--- views/templates_errors/error.php
+++ views/templates_errors/error.php
@@ -1,8 +1,8 @@
-<div class="bg-light text-center d-flex flex-column  vh-100">
-    <div class="container">
+<div class="bg-light text-center d-flex flex-column  justify-content-center align-items-center vh-100">
+    <div class="container text-center">
         <h1 class="display-3 fw-bold"><?php echo $errorCode; ?></h1>
         <h1 class="display-4 pb-3 fw-semibold"><?php echo $errorTitle; ?></h1>
-        <img src="../images/errorFish.png" alt="Error Fish" class="img-fluid my-3 FishError">
+        <img src="../images/errorFish.png" alt="Error Fish" class="img-fluid my-3 FishError" >
         <a href="<?php echo $url; ?>" class="btn btn-primary mt-3 py-3 px-4">Regresar al inicio</a>
     </div>
 </div>
