92a8d26d96551994c0383f8e2a4ce97d87e71784
DiegoNoriega00 <diegonoriega2005@gmail.com>
2024-10-02 21:32:48 -0600

    Se ajustó de manera correcta el menú lateral en el apartado de admin

--- views/layoutAdmin.php
+++ views/layoutAdmin.php
@@ -25,91 +25,93 @@
 
 <body>
 
-  <!-- MENU LATERAL -->
- 
-  <div class="wrapper">
-    <aside id="sidebar">
-      <div class="d-flex">
-        <button id="toggle-btn" type="button">
-          <i class="lni lni-grid-alt"></i>
-        </button>
-        <div class="sidebar-logo">
-          <a href="#">Administrar</a>
+  
+  <div class="main-wrapper">
+    <!-- MENU LATERAL -->
+
+    <div class="wrapper">
+      <aside id="sidebar">
+        <div class="d-flex">
+          <button id="toggle-btn" type="button">
+            <i class="lni lni-grid-alt"></i>
+          </button>
+          <div class="sidebar-logo">
+            <a href="../admin/dashboard">Administrar</a>
+          </div>
         </div>
-      </div>
-      <ul class="sidebar-nav">
-        <li class="sidebar-item">
-          <a href="../admin/dashboard" class="sidebar-link">
-            <i class="lni lni-bar-chart"></i>
-            <span>Resumen</span>
+        <ul class="sidebar-nav">
+          <li class="sidebar-item">
+            <a href="../admin/dashboard" class="sidebar-link">
+              <i class="lni lni-bar-chart"></i>
+              <span>Resumen</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="../admin/Agregar_Productos" class="sidebar-link">
+              <i class="lni lni-gallery"></i>
+              <span>Agregar Productos</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="../admin/Categorias" class="sidebar-link">
+              <i class="lni lni-archive"></i>
+              <span>Categorias</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="../admin/Editar_Productos" class="sidebar-link">
+              <i class="lni lni-pencil-alt"></i>
+              <span>Editar Productos</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="../admin/Ventas" class="sidebar-link">
+              <i class="lni lni-cart"></i>
+              <span>Ventas</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="../admin/Reportes" class="sidebar-link">
+              <i class="lni lni-stats-up"></i>
+              <span>Reportes</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="../admin/Gestion_Usuarios" class="sidebar-link">
+              <i class="lni lni-users"></i>
+              <span>Gestion de Usuarios</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="../admin/Pedidos" class="sidebar-link">
+              <i class="lni lni-delivery"></i>
+              <span>Pedidos</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a href="../admin/Config" class="sidebar-link">
+              <i class="lni lni-cog"></i>
+              <span>Configuración de Pagina</span>
+            </a>
+          </li>
+        </ul>
+        <div class="sidebar-footer">
+          <a href="../auth/logout" class="sidebar-link">
+            <i class="lni lni-exit"></i>
+            <span>Salir</span>
           </a>
-        </li>
-        <li class="sidebar-item">
-          <a href="../admin/Agregar_Productos" class="sidebar-link">
-            <i class="lni lni-gallery"></i>
-            <span>Agregar Productos</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a href="../admin/Categorias" class="sidebar-link">
-            <i class="lni lni-archive"></i>
-            <span>Categorias</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a href="../admin/Editar_Productos" class="sidebar-link">
-            <i class="lni lni-pencil-alt"></i>
-            <span>Editar Productos</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a href="../admin/Ventas" class="sidebar-link">
-            <i class="lni lni-cart"></i>
-            <span>Ventas</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a href="../admin/Reportes" class="sidebar-link">
-            <i class="lni lni-stats-up"></i>
-            <span>Reportes</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a href="../admin/Gestion_Usuarios" class="sidebar-link">
-            <i class="lni lni-users"></i>
-            <span>Gestion de Usuarios</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a href="../admin/Pedidos" class="sidebar-link">
-            <i class="lni lni-delivery"></i>
-            <span>Pedidos</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a href="../admin/Config" class="sidebar-link">
-            <i class="lni lni-cog"></i>
-            <span>Configuración de Pagina</span>
-          </a>
-        </li>
-      </ul>
-      <div class="sidebar-footer">
-        <a href="../auth/logout" class="sidebar-link">
-          <i class="lni lni-exit"></i>
-          <span>Salir</span>
-        </a>
-      </div>
-    </aside>
-  </div>
-
-
-  <!-- FIN MENU LATERAL-->
-  <div class="main-content">
-    <!-- Contenedor para centrar el contenido -->
-    <div class="centered-content">
-      <?php echo $contenido; ?>
+        </div>
+      </aside>
     </div>
-  </div>
+
+
+    <!-- FIN MENU LATERAL-->
+    <div class="main-content">
+      <!-- Contenedor para centrar el contenido -->
+      <div class="centered-content">
+        <?php echo $contenido; ?>
+      </div>
+    </div>
   </div>
 
   <script src="../js/jquery-3.6.0.min.js"></script>
@@ -118,7 +120,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
-    <script src="../js/script.js"></script>
+  <script src="../js/script.js"></script>
 
 
 </body>
