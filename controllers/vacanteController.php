baf092dc35cfcbcb0b1e209522771a5f31d9f649
juan25oo15 <juan.sanchezae@hotmail.com>
2024-11-16 07:48:18 -0600

    Añadir función de eliminar y editar vacante en VacanteController

--- controllers/vacanteController.php
+++ controllers/vacanteController.php
@@ -50,4 +50,63 @@ class VacanteController{
             return;
         }
     }
+
+    public static function eliminarVacante()
+    {
+        header('Content-Type: application/json');
+
+        if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $idVacante = $data['idVacante'];
+
+            // Crear instancia del modelo
+            $vacanteModel = new vacanteModel();
+
+            // Eliminar la vacante
+            $vacanteDelete = $vacanteModel->deleteVacante($idVacante);
+
+            // Redirigir a la vista de vacante o mostrar un error
+            if($vacanteDelete){
+                echo json_encode(['status' => 'success', 'message' => 'Vacante eliminada correctamente.']);    
+            }else {
+                echo json_encode(['status' => 'error', 'message' => 'Error al eliminar la vacante.']);    
+            }
+
+            // Redirigir a la vista de vacante o mostrar un error
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Método no permitido.']);
+            return;
+        }
+        }
+
+        public static function editarVacante()
+        {
+            header('Content-Type: application/json');
+        
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $data = json_decode(file_get_contents('php://input'), true);
+                $idVacante = $data['idVacante'];
+                $nombreVacante = $data['nombreVacante'];
+                $descripcionVacante = $data['descripcionVacante'];
+                $disponibilidadVacante = $data['disponibilidadVacante'];
+        
+                // Crear instancia del modelo
+                $vacanteModel = new vacanteModel();
+        
+                // Editar la vacante
+                $vacanteEdit = $vacanteModel->updateVacante($idVacante, $nombreVacante, $descripcionVacante, $disponibilidadVacante);
+        
+                // Redirigir a la vista de vacante o mostrar un error
+                if($vacanteEdit){
+                    echo json_encode(['status' => 'success', 'message' => 'Vacante editada correctamente.']);
+                }else {
+                    echo json_encode(['status' => 'error', 'message' => 'Error al editar la vacante.']);
+                }
+        
+                // Redirigir a la vista de vacante o mostrar un error
+            } else {
+                echo json_encode(['status' => 'error', 'message' => 'Método no permitido.']);
+                return;
+            }
+    }
 }
