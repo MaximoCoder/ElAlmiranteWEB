7026f6ada08a462ca37308026ef503834571a2c4
Max <maximonaci@gmail.com>
2024-11-18 15:56:05 -0600

    Solo el admin puede acceder a apartado de admin

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,16 +1,18 @@
 <?php
+
 namespace Controllers;
 
 use MVC\Router;
-
+use Controllers\SessionController;
 class AdminController
 {
     // Renderiza las vistas de ADMIN
     public static function renderAdminView(Router $router, $viewName, $layout, $data = [])
     {
+        // Validar que el usuario es administrador
+        SessionController::requireAdmin();
         // Renderizamos la vista pasando solo los datos que sean necesarios
-        $router->render('admin/' . $viewName, $data, $layout); 
+        $router->render('admin/' . $viewName, $data, $layout);
     }
-    
-}
 
+}
